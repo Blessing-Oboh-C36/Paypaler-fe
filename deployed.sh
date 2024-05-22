584cfc545bd1a60aeda526eb7a1c3d1df6810378
queen-bb <blesyn626@gmail.com>
2024-05-22 12:52:52 -0400

    deployed updated by master

--- deployed.sh
+++ deployed.sh
@@ -1,3 +1,4 @@
 Application is ready to be deployed to the development environment.
 Development must be reviewed by managers before deployment.
 Developer2 work is progress
+aws code deploy is a great tool
